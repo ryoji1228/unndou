108def9438564a2b14eea9c33bdcd7639bb1d7da
EC2 Default User <ec2-user@ip-172-31-25-130.ap-northeast-1.compute.internal>
2021-10-15 11:44:06 +0000

    プロフィール画像

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'statics#index'
+  root to: 'statics#index', as: 'statics'
   
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
