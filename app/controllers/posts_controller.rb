3efef16b092a85ecb254ed658bc537652fe939a4
EC2 Default User <ec2-user@ip-172-31-25-130.ap-northeast-1.compute.internal>
2021-10-15 12:37:20 +0000

    投稿

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,14 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @post = Post.new
   end
+  
+  def create
+    post_params = params.require(:post).permit(:text)
+    @post = Post.new(post_params.merge(user_id: current_user.id))
+    @post.save
+    redirect_to '/posts'
+  end
+  
 end
